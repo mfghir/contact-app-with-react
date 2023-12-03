d6ac3de3f9ad09c5ca758a5dd1173561aa2d74a9
mfghir <fatemeghafari77@gmail.com>
2023-12-02 22:45:27 -0800

    showing contact

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,12 +1,28 @@
+// eslint-disable-next-line react/prop-types
 const ContactsList = ({ contacts }) => {
   return (
     <div>
       <h3>Contacts List</h3>
-      <ul>
-        {contacts.map((contact) => (
-          <li key={contact.id} >{contact.name}</li>
-        ))}
-      </ul>
+      {contacts.length ? (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <p>
+                {contact.name} {contact.lastName}
+              </p>
+              <p>
+                <span>📫</span> {contact.email}
+              </p>
+              <p>
+                <span>📞</span> {contact.phone}
+              </p>
+              <button>🗑</button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No contact yet</p>
+      )}
     </div>
   );
 };
